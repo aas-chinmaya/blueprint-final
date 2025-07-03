5137b987d5bc779622d174877b16e4a99044993d
Chinmaya-AAS <it_chinmaya@outlook.com>
2025-07-03 15:51:49 +0530

    final file added

---
+++ src/app/(protected)/task/page.jsx
@@ -0,0 +1,11 @@
+import AllTaskListByRole from "@/components/modules/TaskModules/AllTaskListByRole";
+
+export default function Page() {
+  return (
+    <>
+      <div className="px-4 lg:px-6">
+        <AllTaskListByRole/>
+      </div>
+    </>
+  );
+}
